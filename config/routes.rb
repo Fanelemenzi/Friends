c777fdc61a02217ec8fb6c492b49db8222be2f5e
Fanelesiyibonge Mabuza <menzifm12@gmail.com>
2024-07-08 10:35:16 +0200

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :friends
+  #get 'home/index'
+  root 'home#index'
+  get 'home/about'
+  
+  get "up" => "rails/health#show", as: :rails_health_check
+
+
+end
